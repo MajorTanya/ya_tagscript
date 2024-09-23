5f9d4110ee9399400f11252f9a48c1d0a80da729
MajorTanya <github@majortanya.eu>
2024-09-23 10:33:22 +0200

    Add support for weighted items in RandomBlock
    The weighting logic was copied from Leg3ndary's bTagScript.

--- ya_tagscript/block/randomblock.py
+++ ya_tagscript/block/randomblock.py
@@ -1,3 +1,10 @@
+"""
+Weighting logic copied from Leg3ndary's bTagScript, licensed under Creative Commons
+Attribution 4.0 International License (CC BY 4.0).
+
+cf. https://github.com/Leg3ndary/bTagScript/blob/945b8e34750debea714d36de863412e189975c1b/bTagScript/block/random_block.py
+"""
+
 import random
 from typing import Optional
 
@@ -10,6 +17,7 @@ class RandomBlock(verb_required_block(True, payload=True)):
     Picks a random item from a list of strings, split by either ``~`` or ``,``.
     An optional seed can be provided to the parameter to always choose the same item
     when using that seed.
+    Items can be weighted differently by adding a weight and | before the item(s).
 
     **Usage:** ``{random([seed]):<list>}``
 
@@ -31,6 +39,9 @@ class RandomBlock(verb_required_block(True, payload=True)):
         {=(insult):{#:{insults}}}
         {insult}
         # Assigns a random insult to the insult variable
+
+        {random:5|Cool,3|Lame}
+        # 5 to 3 chances of "Cool" vs "Lame"
     """
 
     ACCEPTED_NAMES = ("random", "#", "rand")
@@ -40,6 +51,16 @@ class RandomBlock(verb_required_block(True, payload=True)):
             spl = ctx.verb.payload.split("~")
         else:
             spl = ctx.verb.payload.split(",")
-        random.seed(ctx.verb.parameter)
 
-        return random.choice(spl)
+        weights = []
+        choices = []
+        for choice in spl:
+            if "|" in choice:
+                weight, choice = choice.split("|", 1)
+                weights.append(int(weight))
+                choices.append(choice)
+            else:
+                weights.append(1)
+                choices.append(choice)
+        random.seed(ctx.verb.parameter)
+        return random.choices(choices, weights, k=1)[0]
