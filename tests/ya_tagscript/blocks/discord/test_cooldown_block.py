5c54a5338ca7ca7387ef28e4f5252ae13b000131
MajorTanya <github@majortanya.eu>
2026-06-29 13:19:05 +0200

    Accommodate flaky cooldown block test
    This test can sometimes fail in CI when the tests run a bit slow under
    coverage, in which case the output mentions "9.0 seconds" instead of
    "10.0 seconds".

--- tests/ya_tagscript/blocks/discord/test_cooldown_block.py
+++ tests/ya_tagscript/blocks/discord/test_cooldown_block.py
@@ -155,7 +155,11 @@ def test_dec_cooldown_custom_message_used_when_exceeding_uses_get_rejected(
     result = ts_interpreter.process(script, data).body
     assert result == ""
     result = ts_interpreter.process(script, data).body
-    assert result == "Cooldown hit! Try again in 10.0 seconds."
+    # can be flaky under coverage due to slower execution, so allow either
+    assert result in (
+        "Cooldown hit! Try again in 10.0 seconds.",
+        "Cooldown hit! Try again in 9.0 seconds.",
+    )
 
 
 def test_dec_cooldown_uses_extra_cooldown_key_kwarg(
